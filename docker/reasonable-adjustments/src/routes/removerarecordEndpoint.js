2384e1322914ae245cfdd228a2d73ca3fddb0562
Adrian Clare <adrian.clare@airelogic.com>
2020-10-02 11:59:55 +0100

    added sandbox container for live proxy

---
+++ docker/reasonable-adjustments/src/routes/removerarecordEndpoint.js
@@ -0,0 +1,10 @@
+const removerarecordPost = {
+    method: 'POST',
+    path: '/removerarecord',
+    handler: (request, h) => {
+        const path = 'removerarecordPOST.json'
+        return h.file(path)
+    }
+};
+
+module.exports = [removerarecordPost]
