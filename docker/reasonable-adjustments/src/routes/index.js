373d5a45d65985b56be91670b6ed455005f45d9b
Adrian Clare <adrian.clare@airelogic.com>
2020-10-13 11:46:34 +0100

    added server error endpoint

--- docker/reasonable-adjustments/src/routes/index.js
+++ docker/reasonable-adjustments/src/routes/index.js
@@ -3,7 +3,9 @@ const flagEndpont = require('./flagEndpoint');
 const listEndpoint = require('./listEndpoint');
 const removerarecordEndpoint = require('./removerarecordEndpoint');
 const statusEndpoint = require('./statusEndpoint');
+const serverError = require('./serverError');
 
-const routes = [].concat(consentEndpoint, flagEndpont, listEndpoint, removerarecordEndpoint, statusEndpoint)
+
+const routes = [].concat(consentEndpoint, flagEndpont, listEndpoint, removerarecordEndpoint, statusEndpoint, serverError)
 
 module.exports = routes
