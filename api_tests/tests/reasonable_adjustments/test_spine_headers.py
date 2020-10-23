37b6414aaba706c4a8b47ddb49887e364ab964a9
Ben Davies <ben.davies@infinityworks.com>
2020-10-23 15:47:10 +0100

    AMB-355 Added inital Spine headers tests and mark

---
+++ api_tests/tests/reasonable_adjustments/test_spine_headers.py
@@ -0,0 +1,40 @@
+import pytest
+from api_tests.config_files import config
+from api_tests.scripts.apigee_api import ApigeeDebugApi
+
+
+@pytest.mark.usefixtures("setup")
+class TestSpineHeadersSuite:
+    """ A test suite to verify the spine headers are sent as part of the target backend request """
+
+    @pytest.mark.spine_headers
+    @pytest.mark.usefixtures('get_token')
+    def test_from_asid_header_is_set(self):
+        # Given
+        debug_session = ApigeeDebugApi(config.REASONABLE_ADJUSTMENTS_PROXY)
+        expected_header_value = 'FromClient'
+
+        # When
+        self.send_a_get_consent_request()
+
+        # Then
+        actual_header_value = debug_session.get_apigee_header('FromASID')
+        assert actual_header_value == expected_header_value
+
+    def send_a_get_consent_request(self):
+        self.reasonable_adjustments.check_endpoint(
+            verb='GET',
+            endpoint='consent',
+            expected_status_code=200,
+            expected_response=None,
+            params={
+                'patient':  'test',
+                'category': 'test',
+                'status':   'test',
+            },
+            headers={
+                'Authorization': f'Bearer {self.token}',
+                'nhsd-session-urid': 'test',
+                'x-request-id': 'test'
+            }
+        )
