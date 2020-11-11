03ba68c281e6de77404e059176e62a6e14f3a313
jalal <jalalhosseiny@gmail.com>
2020-11-11 11:26:29 +0000

    AMB-431 create spineJWT and add to Authorization Header

--- proxies/live/apiproxy/resources/jsc/createClaimVars.js
+++ proxies/live/apiproxy/resources/jsc/createClaimVars.js
@@ -2,3 +2,16 @@ subClaim = context.setVariable('subClaim', 'https://fhir.nhs.uk/Id/sds-role-prof
 requestingSystemClaim = context.setVariable('requestingSystemClaim', 'https://fhir.nhs.uk/Id/accredited-system|' + context.getVariable('verifyapikey.VerifyAPIKey.CustomAttributes.asid'))
 requestingOrganisationClaim = context.setVariable('requestingOrganisationClaim', 'https://fhir.nhs.uk/Id/ods-organization-code|' + context.getVariable('verifyapikey.VerifyAPIKey.CustomAttributes.ods'))
 requestingUserClaim = context.setVariable('requestingUserClaim', 'https://fhir.nhs.uk/Id/sds-role-profile-id|' + context.getVariable('request.header.nhsd-session-urid'))
+scope = "scope here"
+
+jwt = {
+  reason_for_request: "directcare",
+  scope: scope,
+  requesting_organization: requestingOrganisationClaim,
+  requesting_system: requestingSystemClaim,
+  requesting_user: requestingUserClaim
+}
+
+jwt_b64 =btoa(JSON.stringify(jwt))
+
+context.setVariable("spineJwt",jwt_b64)
