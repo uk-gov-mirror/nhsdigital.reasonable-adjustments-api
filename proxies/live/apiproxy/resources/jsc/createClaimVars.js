e1332825aafee842f66b0aef9eadb0b37aeb1560
jalal <jalalhosseiny@gmail.com>
2020-11-11 17:10:33 +0000

    AMB-431 get scope from kvm_scope variable

--- proxies/live/apiproxy/resources/jsc/createClaimVars.js
+++ proxies/live/apiproxy/resources/jsc/createClaimVars.js
@@ -2,7 +2,7 @@ subClaim = 'https://fhir.nhs.uk/Id/sds-role-profile-id|' + context.getVariable('
 requestingSystemClaim = 'https://fhir.nhs.uk/Id/accredited-system|' + context.getVariable('verifyapikey.VerifyAPIKey.CustomAttributes.asid')
 requestingOrganisationClaim = 'https://fhir.nhs.uk/Id/ods-organization-code|' + context.getVariable('verifyapikey.VerifyAPIKey.CustomAttributes.ods')
 requestingUserClaim = 'https://fhir.nhs.uk/Id/sds-role-profile-id|' + context.getVariable('request.header.nhsd-session-urid')
-scope = "user/Consent.read"
+scope = context.getVariable('kvm_scope')
 audience = "/" + context.getVariable('proxy.basepath') + context.getVariable('proxy.pathsuffix')
 
 header = {
