c4b97be6572bff1666862902e7f3bb1bac4bf7cb
Gurdeep Jugpal <gurdeep.jugpal1@nhs.net>
2021-02-18 10:21:32 +0000

    Updated request header names of fromASID and tradeId and removed headers interactionId and toASID

--- api_tests/tests/reasonable_adjustment_flag/test_proxy.py
+++ api_tests/tests/reasonable_adjustment_flag/test_proxy.py
@@ -31,13 +31,13 @@ class TestProxyCasesSuite:
         actual_asid = debug_session.get_apigee_variable('verifyapikey.VerifyAPIKey.CustomAttributes.asid')
         assert_that(expected_value).is_equal_to(actual_asid)
 
-        actual_header_value = debug_session.get_apigee_header('FromASID')
+        actual_header_value = debug_session.get_apigee_header('NHSD-ASID')
         assert_that(expected_value).is_equal_to(actual_header_value)
 
     @pytest.mark.spine_headers
     @pytest.mark.integration
     @pytest.mark.usefixtures('get_token_internal_dev')
-    def test_ToASID_header_is_set(self):
+    def test_x_request_id_equals_nhsd_request_id(self):
         # Given
         debug_session = ApigeeDebugApi(config.REASONABLE_ADJUSTMENTS_PROXY_NAME)
 
@@ -45,21 +45,7 @@ class TestProxyCasesSuite:
         Utils.send_request(self)
 
         # Then
-        actual_header_value = debug_session.get_apigee_header('ToASID')
-        assert_that(actual_header_value).is_not_empty()
-
-    @pytest.mark.spine_headers
-    @pytest.mark.integration
-    @pytest.mark.usefixtures('get_token_internal_dev')
-    def test_x_request_id_equals_TraceID(self):
-        # Given
-        debug_session = ApigeeDebugApi(config.REASONABLE_ADJUSTMENTS_PROXY_NAME)
-
-        # When
-        Utils.send_request(self)
-
-        # Then
-        trace_id = debug_session.get_apigee_header('TraceID')
+        trace_id = debug_session.get_apigee_header('NHSD-Request-ID')
         x_request_id = debug_session.get_apigee_header('x-request-id')
 
         assert_that(trace_id).is_equal_to(x_request_id)
