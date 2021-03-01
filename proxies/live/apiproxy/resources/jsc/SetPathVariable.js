3592457dba1a7dd8e1a56399fa299ca791e144f5
ryan.thomas5 <ryan_j_thomas@hotmail.co.uk>
2021-03-01 14:25:36 +0000

    AMB-332 revert changes to Path in target flow

--- proxies/live/apiproxy/resources/jsc/SetPathVariable.js
+++
@@ -1,8 +0,0 @@
-var xSyncWrapped = context.getVariable('request.header.x-sync-wrapped') 
-var path = "/STU3/RA"
-
-if (!xSyncWrapped) {
-    context.setVariable("Path", path);
-} else {
-    context.setVariable("Path", "");
-}
